0502a29891865c61f70838f465b4528239e1bd85
m-yarbrough978 <MicahYarbrough@my.unt.edu>
2026-09-13 00:09:01 -0500

    Create main.cpp

---
+++ Source Code Files/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+  
+  cout << "+-------------------------------------------+" << endl;
+  cout << "|     Computer Science and Engineering      |" << endl;
+  cout << "|          CSCE 2110 - Project 1            |" << endl;
+  cout << "| Micah Yarbrough micahyarbrough@my.unt.edu |" << endl;
+  cout << "|                                           |" << endl;
+  cout << "+-------------------------------------------+" << endl;
+  
+}
