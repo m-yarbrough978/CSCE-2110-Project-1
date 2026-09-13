17c440c905746e8b2a3cff534997652fc1b7209d
m-yarbrough978 <MicahYarbrough@my.unt.edu>
2026-09-13 00:22:46 -0500

    Create main.cpp
    Created a main file and a rough draft of user interface

---
+++ Source Code Files/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+  
+  cout << "+-------------------------------------------+" << endl;
+  cout << "|     Computer Science and Engineering      |" << endl;
+  cout << "|          CSCE 2110 - Project 1            |" << endl;
+  cout << "| Micah Yarbrough micahyarbrough@my.unt.edu |" << endl;
+  cout << "|                                           |" << endl;
+  cout << "+-------------------------------------------+" << endl << endl;
+
+  cout << "<<<  Campus Resource Reservation System  >>>" << endl;
+  cout << "1. View Resources                           " << endl;
+  cout << "2. Create Reservation                       " << endl;
+  cout << "3. Cancel Reservation                       " << endl;
+  cout << "4. View Waiting List                        " << endl;
+  cout << "5. Undo Cancelation                         " << endl;
+  cout << "6. Search Reservations                      " << endl;
+  cout << "7. Sort Resources                           " << endl;
+  cout << "8. Generate Report                          " << endl;
+  cout << "9. Exit                                     " << endl;
+  cout << "Enter your choice: ";
+  
+  
+}
