3695c1db9a67fba6bebc5d382572212f1c76a3a4
m-yarbrough978 <MicahYarbrough@my.unt.edu>
2026-09-15 00:22:23 -0500

    Update Resource.h
    Declares the Resource class and adds the "#include <string>" line

--- Header Files/Resource.h
+++ Header Files/Resource.h
@@ -3,14 +3,26 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <string>
 
 using namespace std;
 
+class Resource{
+	public:
+		Resource(); //default constructor
+		void SetID(string idX); //Mutator for id
+		string GetID() const; // Accessor for id
+		void SetName(string nameX); //Mutator for name
+		string GetName() const; // Accessor for name
+		void SetType(string typeX); //Mutator for type
+		string GetType() const; // Accessor for type
+		void SetStatus(string statusX); //Mutator for status
+		string GetStatus() const; // Accessor for status
+	
+	private:
+		string id, name, type, status;
 
-
-
-
-
+};
 
 
 
