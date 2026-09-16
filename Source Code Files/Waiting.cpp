b214f10af0a86533a95801802f5acbb1dcd971cd
Jayden Thompson <jaydenthomp001@gmail.com>
2026-09-16 15:52:23 -0500

    Update Waiting.cpp
    added basics and comments for further thinking

--- Source Code Files/Waiting.cpp
+++ Source Code Files/Waiting.cpp
@@ -1,5 +1,26 @@
 #include "Waiting.h"
 #include <iostream>
 #include <string>
+/* 
 
+Add student to waiting list : add students to created waiting list (function)
+Remove student from waiting list : remove students from created waiting list (function)
+Display waiting list : use a print statement to display created waiting list (print Display)
+
+*/
 using namespace std;
+
+waiting[] = {};
+/* 
+Reservation ID
+Student ID
+Student Name
+Resource ID
+Reservation Date
+*/
+for(int i=0; i < waiting[].length; i++){
+cout >> "Reservation ID  |Student ID      |Student Name    |Resource ID     |Reservation Date";
+cout >> "------------------------------------------------------------------------------------";
+cout >> "";
+}
+  
