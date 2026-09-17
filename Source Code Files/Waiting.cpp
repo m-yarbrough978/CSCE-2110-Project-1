6ec2b68d762d2accdc4f69d3650df5409514f322
Jayden Thompson <jaydenthomp001@gmail.com>
2026-09-17 14:25:49 -0500

    Update Waiting.cpp

--- Source Code Files/Waiting.cpp
+++ Source Code Files/Waiting.cpp
@@ -10,17 +10,65 @@ Display waiting list : use a print statement to display created waiting list (pr
 */
 using namespace std;
 
-waiting[] = {};
-/* 
-Reservation ID
-Student ID
-Student Name
-Resource ID
-Reservation Date
-*/
-for(int i=0; i < waiting[].length; i++){
-cout >> "Reservation ID  |Student ID      |Student Name    |Resource ID     |Reservation Date";
-cout >> "------------------------------------------------------------------------------------";
-cout >> "";
+int main() {
+
+Queue<string> waitingList;
+
+int choice;
+string student;
+
+do {
+    cout << "\nWaiting List Management System\n";
+    cout << "1. Add student\n";
+    cout << "2. Remove student\n";
+    cout << "3. Display waiting list\n";
+    cout << "4. Exit\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+  if(choice == 1){
+      cout << "Enter student name: ";
+      cin >> student;
+
+      waitingList.push(student);
+
+      cout << student << " was added to the waiting list.\n";
+  }
+  
+  else if(choice == 2){
+    if(waitingList.empty()){
+      cout << "The waiting list is empty.\n";
+    }
+    else {
+       cout << waitingList.front() << " was removed from the waiting list.\n";
+       waitingList.pop();
+    }
+  } 
+   
+  else if(choice == 3){
+    if(waitingList.empty()){
+      cout << "The waiting list is empty.\n";
+    }
+    else {
+      queue<string> temp = waitingList;
+
+      cout << "\nWaiting List:\n";
+
+      while (!temp.empty()) {
+        cout << temp.front() << endl;
+        temp.pop();
+      }
+    }
+  } 
+  else if(choice == 4){
+    cout << "Goodbye:\n";
+  }
+  else {
+    cout << "Invalid choice.\n";
+  }
+  
+} while (choice != 4);
+     
+return 0;
 }
   
