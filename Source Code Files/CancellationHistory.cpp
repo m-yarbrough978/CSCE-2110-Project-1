5a9ebae47984cbef631ba4ce219b04e045942ef4
Jayden Thompson <jaydenthomp001@gmail.com>
2026-09-19 14:31:57 -0500

    Update CancellationHistory.cpp

--- Source Code Files/CancellationHistory.cpp
+++ Source Code Files/CancellationHistory.cpp
@@ -5,9 +5,56 @@ using namespace std;
 
 void storeCancelledReservation(CancellationHistory& history, Reservation reservation)
 {
+  //doesn't keep old linked-list value
   reservation.next = nullptr;
   history.reservations.push(reservation);
 
   cout << "Reservation " << reservation.id << " was added to cancellation history." << endl;
 }
 
+Reservation restoreReservation(CancellationHistory& history)
+{
+  if(history.reservations.empty()){
+    cout << "There are no cancelled reservations to restore." << endl;
+
+    Reservation emptyReservation = {};
+    return emptyReservation;
+  }
+
+  Reservation restored = history.reservations.top();
+
+  history.reservations.pop();
+  restored.next = nullptr;
+
+  cout << "Reservation " << restored.id << " was restored." << endl;
+
+  return restored;
+}
+
+void displayCancellationHistory(CancellationHistory history)
+{
+  if(history.reservations.empty()){
+    cout << "No cancellation history." << endl;
+    return;
+  }
+
+  cout << "\n--- Cancellation History ---" << endl;
+
+  while(!history.reservations.empty()){
+    Reservation reservation = history.reservations.top();
+
+    cout << "Reservation ID: " << reservation.id << endl;
+
+    cout << "Student: " << reservation.studentName << endl;
+
+    cout << "Resource ID: " << reservation.resourceID << endl;
+
+    cout << "Date: " << reservation.date << endl;
+
+    cout << "Time: " << reservation.time << endl;
+
+    cout << endl;
+
+    history.reservations.pop();
+  }
+}
