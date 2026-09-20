92255037a884485635314823d13c386988d0aa65
Jayden Thompson <jaydenthomp001@gmail.com>
2026-09-20 00:32:41 -0500

    Refactor Reservation class with additional includes

--- Source Code Files/Reservation.cpp
+++ Source Code Files/Reservation.cpp
@@ -1,6 +1,8 @@
 #include "Reservation.h"
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <sstream>
 Reservation::Reservation()
     : reservationId(0), studentId(""), resourceId(""), date(""),
       startHour(0), endHour(0) {
